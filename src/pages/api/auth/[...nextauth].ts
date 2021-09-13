53b7f617aaaf639a8a1073d4c05d43f4fb3786c9
Gyubido <gubarcaro@gmail.com>
2021-09-13 18:11:10 -0300

    login

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,42 @@
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: "read:user",
+    }),
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: {
+              email,
+            },
+          })
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
+
+// FaunaDB - http
+// DynamoDb -await
+
+// PostgresSQL, mongoDB
+
+// 24h (1 conexão)
+// 1000 autenticação ()
